3d97bfab913aeee21bafbc4b291c79b8ac3a1310
Brad Slayter <>
2019-07-26 13:48:28 -0500

    Add settings page

--- src/Routes.js
+++ src/Routes.js
@@ -9,12 +9,14 @@ import NewNote from './containers/NewNote';
 import Notes from './containers/Notes';
 import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 import AuthenticatedRoute from './components/AuthenticatedRoute';
+import Settings from './containers/Settings';
 
 export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
         <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+        <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
         <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
         <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
         <Route component={NotFound} />
--- src/config.js
+++ src/config.js
@@ -1,17 +1,42 @@
-export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
     s3: {
         REGION: 'us-east-1',
-        BUCKET: 'slayterdev-notes-uploads'
+        BUCKET: 'notes-app-2-api-dev-attachmentsbucket-3ftoqzdf39xc'
     },
     apiGateway: {
         REGION: 'us-east-1',
-        URL: 'https://z9w7ks8kw2.execute-api.us-east-1.amazonaws.com/prod'
+        URL: 'https://z2snr4dgmd.execute-api.us-east-1.amazonaws.com/dev'
     },
     cognito: {
         REGION: 'us-east-1',
-        USER_POOL_ID: 'us-east-1_lP3v0JsRr',
-        APP_CLIENT_ID: '3hl5nioeqhovre1lu752rvkae5',
-        IDENTITY_POOL_ID: 'us-east-1:6406d2b2-5c54-41c9-a6fb-5fe22d35c1de'
+        USER_POOL_ID: 'us-east-1_d4XUuY0oI',
+        APP_CLIENT_ID: '53u7oqdfdv1uj685oq4ge7251n',
+        IDENTITY_POOL_ID: 'us-east-1:7a25333c-45f9-4df6-90cc-e92eebb36537'
     }
+};
+
+const prod = {
+    s3: {
+        REGION: 'us-east-1',
+        BUCKET: 'notes-app-2-api-prod-attachmentsbucket-1uii70vvhejx2'
+    },
+    apiGateway: {
+        REGION: 'us-east-1',
+        URL: 'https://1vr4n4o9fb.execute-api.us-east-1.amazonaws.com/prod'
+    },
+    cognito: {
+        REGION: 'us-east-1',
+        USER_POOL_ID: 'us-east-1_eBUdqxDXX',
+        APP_CLIENT_ID: 'e4s0pp8agt72rblv70ckdvr7v',
+        IDENTITY_POOL_ID: 'us-east-1:1d361eed-0c78-40bc-a59b-441d4aeaa5b1'
+    }
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
